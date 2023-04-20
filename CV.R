01f278b612ed40af8fb6b03a5dc2413a4dab89cb
Shiya <sherylliu2018@gmail.com>
2023-04-19 21:45:01 -0700

    Update

---
+++ CV.R
@@ -0,0 +1,7 @@
+datadrivencv::use_datadriven_cv(
+  full_name = "Shiya Liu",
+  data_location = "https://docs.google.com/spreadsheets/d/1mbfBsfu2mmLDygZuURVKmJ99lp3D_fEO6829nOtSDOc/edit#gid=917338460",
+  pdf_location = "https://shiya-liu.github.io/CV/cv.pdf",
+  html_location = "https://shiya-liu.github.io/CV/cv.html",
+  source_location = "https://github.com/shiyaliu/cv"
+)
